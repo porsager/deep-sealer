493ebb37521711893d1fb26bb50a8537d4830dea
Rasmus Porsager <rasmus@porsager.com>
2019-01-22 23:40:49 +0100

    improve browser + cjs

--- index.browser.js
+++
@@ -1,7 +0,0 @@
-window.deepSealer = function deepSealer(o) {
-  Object.keys(o).forEach(key => {
-    if (!Array.isArray(o[key]) && typeof o[key] === 'object' && o[key] !== null)
-      o[key] = deepSealer(o[key])
-  })
-  return Object.seal(o)
-}
--- index.legacy.js
+++ index.legacy.js
@@ -1,7 +1,10 @@
-module.exports = function deepSealer(o) {
+;function deepSealer(o) {
   Object.keys(o).forEach(key => {
     if (!Array.isArray(o[key]) && typeof o[key] === 'object' && o[key] !== null)
       o[key] = deepSealer(o[key])
   })
   return Object.seal(o)
 }
+typeof module !== 'undefined'
+  ? module.exports = deepSealer
+  : window.deepSealer = deepSealer;
